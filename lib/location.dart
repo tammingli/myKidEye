389e9fdf58a245c60c4e1de284f197350be8dfb2
tammingli <91863456+tammingli@users.noreply.github.com>
2021-11-17 15:31:17 +0800

    Update location.dart

--- lib/location.dart
+++ lib/location.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:map_launcher/map_launcher.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 import 'navigation_drawer.dart';
 
 class GoogleMapScreen extends StatefulWidget {
@@ -8,40 +10,71 @@ class GoogleMapScreen extends StatefulWidget {
 }
 
 class _GoogleMapScreenState extends State<GoogleMapScreen>{
-  Set<Marker>_markers ={};
 
-  void _onMapCreated(GoogleMapController controller){
-    setState(() {
-      _markers.add(
-        Marker(
-          markerId: MarkerId('id-1'),
-          position: LatLng(3.223902,101.730243),
-          infoWindow: InfoWindow(
-            title: 'Our Optometris Vision Care',
-            snippet: 'Vision Care',
-          ),
-      ),
-      );
-    });
-  }
 
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lokasi'),
+        title: Text('Lokasi Kami'),
         backgroundColor: Colors.blue[300],
       ),
       drawer: new NavigationDrawer(),
-      body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          markers: _markers,
-          initialCameraPosition: CameraPosition(target:LatLng(3.223902,101.730243),
-            zoom: 15,
-          ),
-        ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width ,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('image/lokasi.jpg'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: Text(
+                    'Our Optometris Vision Care', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 50.0,
+                child: InkWell(
+                  onTap: () async {
+                    bool isGoogleMapAvailable =
+                    await MapLauncher.isMapAvailable(MapType.google);
+                    if(isGoogleMapAvailable){
+                      MapsLauncher.launchCoordinates(
+                          3.223902,101.730243);
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      color: Colors.blue[200],
+                    ),
+                    child: Center(
+                      child: Text('Peta', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        )
+      )
     );
 
   }
 }
-  
